9b275a08adc432f0ddc178e77bee399bad02c70a
Arno Pilvar <arno.pilvar@ut.ee>
2024-03-27 12:17:02 +0200

    Work in progress

--- src/CMD.java
+++ src/CMD.java
@@ -1,9 +1,21 @@
-//Ei tööta, WORK IN PROGRESS
+//WORK IN PROGRESS
 //https://stackoverflow.com/questions/2979383/how-to-clear-the-console-using-java
 import java.io.IOException;
 
 public class CMD {
-    public static void cls() throws IOException, InterruptedException {
-        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+    public static void cls() {
+//        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        System.out.println("\n".repeat(50));
+    }
+    //tasklist.exe /fo csv /nh
+    public static void tasklist() throws IOException, InterruptedException {
+        new ProcessBuilder("cmd", "/c", "tasklist.exe /fo csv /nh").inheritIO().start().waitFor();
+    }
+}
+class testCMD{
+    public static void main(String[] args) throws IOException, InterruptedException {
+        CMD.tasklist();
+        CMD.cls();
+        System.out.println("Testrida");
     }
 }
--- src/Setup.java
+++ src/Setup.java
@@ -1,18 +1,75 @@
 //WORK IN PROGRESS
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Setup {
     public static void LooRakendusedTXT () throws IOException, InterruptedException {
         //kaardista programmid
         List<String> programmid = new KaardistaProgrammid().getProgrammid();
-        System.out.println("test");
-        CMD.cls();
-        //küsi kasutajalt millised programmid nimekirjast eemaldada
+        Scanner reaLugeja = new Scanner(System.in);
+        String kasutajaValik = "";
 
+        System.out.print("Järgnevalt väljastatakse kõik leitud kasutaja rakendused (Enter):");
+        kasutajaValik = reaLugeja.nextLine();
+        String s6num = "\nEemalda neutraalsed rakendused, mis ei mõjuta produktiivsust sisestades rakenduse numbri" +
+                "\nJärgmisesse etappi liikumiseks sisesta 'jätka'" +
+                "\n sisend: ";
+        esitaProgrammid(programmid);
+        //küsi kasutajalt millised programmid nimekirjast eemaldada
         //kuni vastuseni "jätka"
+        System.out.print(s6num);
+        kasutajaValik = "";
+        while(!Objects.equals(kasutajaValik, "jätka")){
+            kasutajaValik = reaLugeja.nextLine();
+            if(kasutajaValik.equals("jätka")) continue;
+            try{
+                int valik = Integer.parseInt(kasutajaValik);
+                if(valik < 0 || valik > programmid.size()) throw (new Exception());
+                programmid.remove(valik-1);
+            }catch (Exception e) {
+                System.out.print("Vale sisend. Proovi uuesti: ");
+                continue;
+            }
+            CMD.cls();
+            esitaProgrammid(programmid);
+            System.out.print(s6num);
+        }
+
         //küsi millised programmidest on produktiivsed
         //kuni vastuseni "jätka"
-        //loo fail programmid.txt
+        List<String> produktiivsed = new ArrayList<>();
+        s6num = "Vali millised programmidest on produktiivsed sisestades programmi numbri";
+        System.out.println(s6num);
+        kasutajaValik = "";
+        while(!Objects.equals(kasutajaValik, "jätka")){
+            kasutajaValik = reaLugeja.nextLine();
+            if(kasutajaValik.equals("jätka")) continue;
+            try{
+                int valik = Integer.parseInt(kasutajaValik);
+                if(valik < 0 || valik > programmid.size()) throw (new Exception());
+                programmid.remove(valik-1);
+            }catch (Exception e) {
+                System.out.print("Vale sisend. Proovi uuesti: ");
+                continue;
+            }
+            CMD.cls();
+            esitaProgrammid(programmid);
+            System.out.print(s6num);
+        }
+
+        //salvesta tulemus faili rakendused.txt
+    }
+
+    private static void esitaProgrammid(List<String> programmid){
+        int i = 1;
+        String nimi = "";
+        String exeNimi = "";
+        for (String exe : programmid) {
+            exeNimi = exe.substring(exe.lastIndexOf('\\') + 1);
+            System.out.println("("+i+++") "+exeNimi);
+        }
     }
 }
