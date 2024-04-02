ef3c4e6173bdc09e72c44fcc06586408d1bf5bb1
Arno Pilvar <arno.pilvar@ut.ee>
2024-04-02 22:03:02 +0300

    Viimane lihv

--- src/JärjekordneProduktiivsuseÄpp.java
+++ src/JärjekordneProduktiivsuseÄpp.java
@@ -1,6 +1,5 @@
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -19,26 +18,28 @@ public class JärjekordneProduktiivsuseÄpp {
 
         //UI
         Scanner reaLugeja = new Scanner(System.in); //lähesta kasutajaliidese sisend
-        String kastajaValik = "";
+        String kasutajaValik;
         String päis = " Järjekordne Produktiivsuse Äpp\n" + //salvestame menüü hilisemaks kasutamiseks
                 "--------------------------------\n";
         String ridaA = "1. alusta jälgimist\n";
         String ridaB = "1. peata jälgimine\n";
-        String valikud = "2. \"Im feeling lucky\" - ava juhuslik rakendus\n" +
-                "3. näita rakendusi\n" +
-                "4. näita seisu\n" +
-                "5. tee setup uuesti\n" +
-                "6. EXIT\n" +
-                "--------------------------------\n" +
-                "tee valik: ";
+        String valikud = """
+                2. "Im feeling lucky" - ava juhuslik rakendus
+                3. näita rakendusi
+                4. tee setup uuesti
+                5. EXIT
+                --------------------------------
+                """;
         String menüüA = päis + ridaA + valikud; //menüü juhuks kui taskmonitor seisab
         String menüüB = päis + ridaB + valikud; //menüü juhuks kui taskmonitor on aktiivne
         Abi.cls(); //genereeri 50 tühja rida, ehk puhasta konsool
         System.out.print(menüüA);
-        A: // lipp, et programmist väljuda (et 'break' kehtiks while loopi kohta)
+        näitaSeisu(taskmonitor);
+        System.out.print("tee valik: ");
+        A: // silt, et programmist väljuda (et 'break' kehtiks while loopi kohta)
         while (true) { //peamenüü loop
-            kastajaValik = reaLugeja.nextLine(); // kasutaja sisend
-            switch (kastajaValik) {
+            kasutajaValik = reaLugeja.nextLine(); // kasutaja sisend
+            switch (kasutajaValik) {
                 case "1": { // 1. alusta jälgimist / peata jälgimine
                     Abi.cls();
                     if (!taskmonitor.isRunning()) { // kui taskmonitor ei tööta -
@@ -49,6 +50,8 @@ public class JärjekordneProduktiivsuseÄpp {
                         taskmonitor.stop(); // peata taskmonitor
                         System.out.print(menüüA);
                     }
+                    näitaSeisu(taskmonitor);
+                    System.out.print("tee valik: ");
                     continue;
                 }
                 case "2": { // 2. "Im feeling lucky"
@@ -64,38 +67,49 @@ public class JärjekordneProduktiivsuseÄpp {
                     System.out.println("\nProduktiivsed");
                     Abi.esitaProgrammidA(programmid.subList(ebaproduktiivseid - 1, programmid.size()), ebaproduktiivseid); //esita programmide alamnimekiri kus on ainult produktiivsed, jätka nummerdamist viimasest ebaproduktiivsest
                     System.out.println("rakenduse avamiseks sisesta rakendusele vastav number");
-                    System.out.print("menüüsse naasmiseks vajuta Enter");
+                    System.out.println("menüüsse naasmiseks vajuta Enter");
+                    System.out.print("tee valik: ");
+
                     while (true) {
-                        kastajaValik = reaLugeja.nextLine();
-                        if (kastajaValik.isEmpty()) { // kasutaja sisestas Enter
+                        kasutajaValik = reaLugeja.nextLine();
+                        if (kasutajaValik.isEmpty()) { // kasutaja sisestas Enter
                             Abi.cls();
-                            if (taskmonitor.isRunning())
+                            if (taskmonitor.isRunning()) {
                                 System.out.print(menüüB);
-                            else
+                            }
+                            else {
                                 System.out.print(menüüA);
-                            break;
+                            }
+                            näitaSeisu(taskmonitor);
+                            System.out.print("tee valik: ");
+                            continue A;
                         }
                         else {
                             try {
                                 int valik;
-                                valik = Integer.valueOf(kastajaValik);
+                                valik = Integer.parseInt(kasutajaValik);
                                 if (valik > programmid.size() || valik < 1) { // kasutaja valik on massiivist väljas
                                     System.out.print("Vigane sisend. Proovi uuesti: ");
                                 } else if (taskmonitor.getSkoor() < 1 && programmid.get(valik - 1)[2].equals("0")) { // kasutaja tahab avada ebaproduktiivset rakendust, kui skoor on alla nulli
                                     System.out.println("\nTeie skoor on liiga madal ebaproduktiivsete rakenduste kasutamiseks!");
                                     System.out.println("Skoori suurendamiseks veeda aega produktiivsetes rakendustes.");
                                     System.out.println("\nrakenduse avamiseks sisesta rakendusele vastav number");
-                                    System.out.print("menüüsse naasmiseks vajuta Enter: ");
+                                    System.out.println("menüüsse naasmiseks vajuta Enter");
+                                    System.out.print("tee valik: ");
                                 } else {
                                     System.out.print("Peagi avatakse rakendus: " + programmid.get(valik - 1)[0]);
                                     Abi.käivita(programmid.get(valik - 1)[1]);
                                     Thread.sleep(4000); // annab arvutile aega rakendus avada
                                     Abi.cls();
-                                    if (taskmonitor.isRunning())
+                                    if (taskmonitor.isRunning()) {
                                         System.out.print(menüüB);
-                                    else
+                                    }
+                                    else {
                                         System.out.print(menüüA);
-                                    break;
+                                    }
+                                    näitaSeisu(taskmonitor);
+                                    System.out.print("tee valik: ");
+                                    continue A;
                                 }
                             } catch (NumberFormatException e) {
                                 System.out.print("Vigane sisend. Proovi uuesti: ");
@@ -105,25 +119,30 @@ public class JärjekordneProduktiivsuseÄpp {
                         }
                     }
                 }
-                case "4": { // 4. näita seisu
-                    if (taskmonitor.isRunning()) System.out.print("Jälgimine: aktiivne; ");
-                    else System.out.print("Jälgimine: seisab; ");
-                    System.out.println("Hetke skoor: " + taskmonitor.getSkoor());
-                    System.out.print("tee valik: ");
-                    continue;
-                }
-                case "5": { // 5. tee setup uuesti
+//                case "4": { // 4. näita seisu
+//                    if (taskmonitor.isRunning()) System.out.print("Jälgimine: aktiivne; ");
+//                    else System.out.print("Jälgimine: seisab; ");
+//                    System.out.println("Hetke skoor: " + taskmonitor.getSkoor());
+//                    System.out.print("tee valik: ");
+//                    continue;
+//                }
+                case "4": { // 5. tee setup uuesti
 
                     Abi.cls();
                     Setup.LooRakendusedTXT();
                     Abi.cls();
-                    if (taskmonitor.isRunning())
+                    if (taskmonitor.isRunning()) {
                         System.out.print(menüüB);
-                    else
+                    }
+                    else {
                         System.out.print(menüüA);
+                    }
+                    näitaSeisu(taskmonitor);
+                    System.out.print("tee valik: ");
+
                     continue;
                 }
-                case "6": { // 6. EXIT
+                case "5": { // 6. EXIT
                     taskmonitor.stop();
                     break A;
                 }
@@ -132,6 +151,11 @@ public class JärjekordneProduktiivsuseÄpp {
                 }
             }
             }
+        }
+        private static void näitaSeisu(TaskMonitor taskmonitor){
+            if (taskmonitor.isRunning()) System.out.print("Jälgimine: aktiivne; ");
+            else System.out.print("Jälgimine: seisab; ");
+            System.out.println("Hetke skoor: " + taskmonitor.getSkoor());
         }
 
     }
